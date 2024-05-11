3f9b72f723787414906d3b02ba00278b46f9b117
Anuji Perera <anujiruwandi5@gmail.com>
2024-05-11 13:45:34 +0530

    create view Notes Items

--- Notes/app/src/main/java/com/example/notes/MainActivity.kt
+++ Notes/app/src/main/java/com/example/notes/MainActivity.kt
@@ -3,25 +3,38 @@ package com.example.notes
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NotesDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NotesDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(),this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
        binding.addbutton.setOnClickListener{
         val intent = Intent(this, AddNoteActivity::class.java)
        startActivity(intent)
      }
+    }
 
-
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
